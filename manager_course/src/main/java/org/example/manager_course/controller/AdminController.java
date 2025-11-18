ebe11fb60b224cdc24dfeec0e6e605f8b88c4698
HoangLeoO <leoo09012001@gmail.com>
2025-11-18 09:10:44 +0700

    hoang tao controller

---
+++ manager_course/src/main/java/org/example/manager_course/controller/AdminController.java
@@ -0,0 +1,8 @@
+package org.example.manager_course.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name ="AdminController",value = "/admins")
+public class AdminController extends HttpServlet {
+}
---
+++ manager_course/src/main/java/org/example/manager_course/controller/AttendanceController.java
@@ -0,0 +1,4 @@
+package org.example.manager_course.controller;
+
+public class AttendanceController {
+}
